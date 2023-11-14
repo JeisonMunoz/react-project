41934f1b6bffbf43ea832885e539c1526e2e06cc
Jeison Munoz <jmunozbetancourt@gmail.com>
2023-11-14 07:01:21 -0500

    se encapsulo App.js dentro de React.Fragment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <React.Fragment>
       <TodoCounter completed={16} total={25}/>
       <TodoSearch/>
 
@@ -21,7 +22,7 @@ function App() {
 
       <CreateTodoButton/>
 
-    </div>
+    </React.Fragment>
   );
 }
 
